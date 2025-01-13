d2f3805e3f78a0cb32e8facda68ab9cb4486f27b
radekaadek <radekaadek@gmail.com>
2025-01-13 10:21:32 +0100

    Wspolrzedne punktow granicznych

--- 3/main.py
+++ 3/main.py
@@ -13,7 +13,7 @@ def random_color():
     return "#{:06x}".format(random.randint(0, 0xFFFFFF))
 
 # add EGB_ to the list
-name_to_pos = {'Budynek': 3, 'DzialkaEwidencyjna': 2, 'KonturKlasyfikacyjny': 1, 'KonturUzytkuGruntowego': 0}
+name_to_pos = {'PunktGraniczny':4, 'Budynek': 3, 'DzialkaEwidencyjna': 2, 'KonturKlasyfikacyjny': 1, 'KonturUzytkuGruntowego': 0}
 # add EGB_ to every key
 name_to_pos = {f"EGB_{key}": value for key, value in name_to_pos.items()}
 datas = [None] * len(name_to_pos)
@@ -28,22 +28,24 @@ for idx, layer in gpd.list_layers("Zbiór danych GML ZSK 2025.gml").iterrows():
             # add name as a column
             data['layer'] = name
             data['color'] = random_color()
+            # if its PunktGraniczny add wspolrzedne column with geometry
+            # if name == "EGB_PunktGraniczny":
+            #     data['Wspolrzedne'] = data['geometry']
             datas[name_to_pos[name]] = data
             data_reproj = data.to_crs(epsg=4326)
     else:
         data.to_csv(f"{data_dir}/{name}.csv", index=False)
-
-
-# function to create a popup with feature's attributes
-def create_popup(feature):
-    popup_content = "<br>".join([f"{key}: {value}" for key, value in feature['properties'].items()])
-    return folium.Popup(popup_content, max_width=300)
 
 # visualize using folium
 m = folium.Map(location=[52.26520441814408, 20.55219304492736], zoom_start=13)
 
 for data in datas:
     fields = [key for key in data.columns if key != 'geometry']
+    # print(data['geometry'])
+    data['Wspolrzedne'] = ''
+    for idx, row in data.iterrows():
+        data.loc[idx, 'Wspolrzedne'] = str(row['geometry'])[7:-1]
+    fields.append('Wspolrzedne')
     #get current layer name
     layer_name = data.iloc[0].layer
     geojson_layer = folium.GeoJson(
