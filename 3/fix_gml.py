698d15bd995d52a1692479c1711ab4048b162ac6
radekaadek <radekaadek@gmail.com>
2025-01-13 13:17:55 +0100

    more updates to gml fixer

--- 3/fix_gml.py
+++ 3/fix_gml.py
@@ -4,6 +4,7 @@ output_file = 'Fixed.gml'
 with open(input_file, 'r') as f:
     lines = f.readlines()
 
+#### UDZIAL WE WLASNOSCI
 # delete all lineswith <egb:podmiotUdzialuWlasnosci>
 for i in range(len(lines)):
     if '<egb:podmiotUdzialuWlasnosci>' in lines[i]:
@@ -36,10 +37,22 @@ for i in range(len(lines)):
 # to
 # <egb:podstawaUtworzeniaWersjiObiektu>PL.PZGiK.179.EGiB_aadb7f8f-066b-44e6-ae19-0994cbea4448_2007-06-04T11-36-35</egb:podstawaUtworzeniaWersjiObiektu>
 for i in range(len(lines)):
-    if r'<egb:podstawaUtworzeniaWersjiObiektu xlink:href="' in lines[i] and r'' == lines[i][-2:]:
+    if r'<egb:podstawaUtworzeniaWersjiObiektu xlink:href="' in lines[i]:
         id = lines[i].split('"')[1]
         lines[i] = f'<egb:podstawaUtworzeniaWersjiObiektu>{id}</egb:podstawaUtworzeniaWersjiObiektu>\n'
 
+
+
+#### OsobaFizyczna
+# change all <egb:adresOsobyFizycznej xlink:href="PL.PZGiK.179.EGiB_8e0cc5e9-d059-459a-93c7-48f957a8d09d_2022-10-13T12-17-41" xlink:type="simple"/>
+# to <egb:adresOsobyFizycznej>PL.PZGiK.179.EGiB_8e0cc5e9-d059-459a-93c7-48f957a8d09d_2022-10-13T12-17-41</egb:adresOsobyFizycznej>
+for i in range(len(lines)):
+    # print(f"ending: {lines[i][-3:-1]}")
+    if r'<egb:adresOsobyFizycznej xlink:href="' in lines[i] and lines[i][-3:-1] == '/>':
+        id = lines[i].split('"')[1]
+        lines[i] = f"<egb:adresOsobyFizycznej>{id}</egb:adresOsobyFizycznej>\n"
+
+#### adresZameldowania jest ok
 
 # write to Fixed.gml
 with open(output_file, 'w') as f:
